ebcd726698efd756ab7875ce7e3f7eb0f1de5b08
hiroki338 <michaelweex@gmail.com>
2023-12-20 21:51:54 +0800

    Update website with new changes

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// deployed contract address
-const contractAddress = '0x20faFA618513A3A8690D7e18103b104Dbc01153F'; 
-// ABI from remix
-const contractABI = [
+const contractAddress = "0x4B0233BE781380Ed94204337EE5F261ba854bD54";
+const abi = [
 	{
 		"inputs": [],
 		"name": "enter",
@@ -193,145 +191,93 @@ const contractABI = [
 		"type": "function"
 	}
 ];
-
-
-let walletAddress = null;
-let provider = null;
-let signer = null;
-let contract = null;
-// const decimal= 10**18;
-
-async function connect() {
-    if (window.ethereum) {
+let contract;
+let web3;
+let accounts;
+ 
+window.addEventListener('load', async () => {
+    if(window.ethereum) {
+        web3 = new Web3(window.ethereum);
         try {
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            walletAddress = (await window.ethereum.request({ method: 'eth_accounts' }))[0];
-            document.getElementById('connectWallet').innerText = 'Connected';
+            await window.ethereum.enable();
+            initApp();
+        } catch (error) {
+            console.error("Access to your Ethereum account rejected.");
+        }
+    } 
+	else {
+        console.error("Please install MetaMask!");
+		alert("Please Install metamask");
+    }
+});
+ 
+function initApp() {
+    contract = new web3.eth.Contract(abi, contractAddress);
+    document.getElementById('connectWallet').addEventListener('click', async () => {
+        accounts = await web3.eth.getAccounts();
+		manageraddress = await contract.methods.manager().call();
+        document.getElementById('connectWallet').innerText = 'Connected';
             if (accounts && accounts.length > 0) {
                 const truncatedAddress = `${accounts[0].slice(0, 5)}...${accounts[0].slice(-5)}`;
                 document.getElementById('connectWallet').innerText = `Connected (${truncatedAddress})`;
                 document.getElementById('connectWallet').disabled = true;
-            }
-            //provider = new ethers.providers.Web3Provider(window.ethereum);
-            provider = new ethers.getDefaultProvider(network)
-            signer = provider.getSigner();
-            contract = new ethers.Contract(contractAddress, contractABI, signer);
-            console.log('Wallet Connected');
+		
+        
+            console.log('Wallet Connected',accounts[0]);
             console.log("First 5 characters:", accounts[0].slice(0, 5));
             console.log("Last 5 characters:", accounts[0].slice(-5));
-        } 
-        catch (error) {
-            console.error(error);
-            document.getElementById('connectWallet').innerText = 'Wallet Connection Failed';
-        }
-    } else {
-        alert('MetaMask extension not detected. Please install MetaMask and try again.');
-    }
-}
+		alert("Wallet Connected");
+		
+    }});
 
-//function to use enter() function
-async function enterLottery() {
-    contract.enter();
-    const tx = signer.sendTransaction({
-        to: "0xE5Dc013d8002Cf9c2c8921D3c15Ab8B48013b42E",
-        value: ethers.utils.par,
-    
+
+    document.getElementById('enterLottery').addEventListener('click', () => {
+        contract.methods.enter().send({ from: accounts[0], value: web3.utils.toWei("0.01", "ether") })
+        .then(() => console.log("Entered the lottery!"))
+        .catch(console.error);
+		alert("Lottery Entered, click CONFIRM");
+    });
+ 
+
+    document.getElementById('pickWinner').addEventListener('click', () => {
+      let tx1 = contract.methods.getParticipants().call();
+	  let tx2 = tx1.length;
+	  let tx3 = tx2*10**16;
+      contract.methods.pickWinner().send({ from: accounts[0], value: tx3})
+        .then(() => console.log("Winner picked!"))
+        .catch(console.error);
+		alert("Winner Picked");
     });
 
-      alert("You enter Lottery. Good Luck!");
+
+     // Fetch and display participants
+	 contract.methods.getParticipants().call()
+	 .then(displayParticipants)
+	 .catch(console.error);
+
+}
+  
+
+   function displayParticipants(participants) {
+    const participantsList = document.getElementById('participantsList');
+    participantsList.innerHTML = participants.map(address => `<li>${address}</li>`).join('');}
+
+
+async function test(){
+  let aa = await contract.methods.getParticipants().call();
+let bb= aa.length;
+let cc = tx2*10**16;
+  alert(aa);
+  alert(bb);
+  alert(cc);
 }
 
-// Function to check if the current user is the owner or manager
-async function isOwnerOrManager() {
-    try {
-        // Get the owner and manager addresses from the contract
-        const owner = await contract.Owner();
-        const manager = await contract.manager();
-
-        // Compare the owner and manager addresses with the current user's address
-        return walletAddress === owner || walletAddress === manager;
-    } catch (error) {
-        console.error('Error checking if user is owner or manager:', error);
-        return false;
-    }
-}
-
-// Function to pick the winner and display winner information
-async function pickWinner() {
-    try {
-        // Check if the wallet is connected
-        if (walletAddress) {
-            // Check if the current user is the owner or manager
-            const isOwnerOrManager = await isOwnerOrManager();
-
-            if (isOwnerOrManager) {
-                // Continue with the existing logic to pick the winner and display information
-                const tx = await contract.pickWinner({ from: walletAddress });
-                await tx.wait();
-                console.log('Winner picked');
-
-                const [winner, winnings] = await contract.getWinnerInfo();
-                document.getElementById('winner').innerText = `Winner Address: ${winner}`;
-                document.getElementById('winnings').innerText = `Winnings: ${ethers.utils.formatEther(winnings)} ETH`;
-            } else {
-                alert('You are not authorized to pick the winner.');
-            }
-        } else {
-            alert('Wallet not connected. Please connect your wallet.');
-        }
-    } catch (error) {
-        console.error('Error picking winner:', error);
-    }
-}
-
-// Example event listener for the "Pick Winner" button
-document.getElementById('pickWinner').addEventListener('click', getWinnerInfo);
-
-
-
-//function to use pickWinner() function
-async function pickWinner() {
-    try {       
-        if (walletAddress) {
-            // Call the "pickWinner" function on your contract
-            const isOwnerOrManager = await isOwnerOrManager();
-        if (isOwnerOrManager) {    
-            const tx = await contract.pickWinner({ from: contractAddress });
-            await tx.wait();
-            console.log('Winner picked');
-        } else {
-            alert('Wallet not connected. Please connect your wallet.');
-        }
-        } catch (error) {
-        console.error('ERROR picking winner:', error);
-    }
-}
-}
-
-
-// Function to pick the winner
-async function getWinner() {
-    try {
-        // Call the getWinnerInfo function from the contract
-        const [winner, winnings] = await contract.getWinnerInfo();
-
-        // Display winner information on the HTML page
-        document.getElementById('winner').innerText = `Winner Address: ${winner}`;
-        document.getElementById('winnings').innerText = `Winnings: ${ethers.utils.formatEther(winnings)} ETH`;
-    } catch (error) {
-        console.error('Error picking winner:', error.message);
-    }
-}
-
-
-//function to show players with getParticipants function call
-async function getshowparticipants(){
-try{
-displayPlayers = await contract.getParticipants();
-document.getElementById('participantsList').innerText = displayPlayers;
-console.log('Get Participants SUCCESS');
-}
-catch(error){
-    console.error('ERROR Getting Participants', error);
-}
+async function randomWinner(){
+	let tx1 = await contract.methods.getParticipants().call();
+	  let tx2 = tx1.length;
+	  let tx3 = tx2*10**16;	
+	contract.methods.pickWinner().send({ from: accounts[0], value: tx3})
+        .then(() => console.log("Winner picked!"))
+        .catch(console.error);
+		alert("Winner Picked");
 }
